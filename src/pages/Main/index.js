713abb1e0c09c1710f4374cb83ff00f5d0cb926c
Samuel Brehm <samuelbrehm@gmail.com>
2019-05-13 15:05:55 -0300

    Cards criados

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
-      
-      <Tabs/>
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponível</Title>
+            <Description>R$ 203.014,95</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Samuel Brehm Porto hoje às 14:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
     </Container>
   );
 }
